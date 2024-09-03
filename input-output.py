dd83530a574b3e931f01c63e81165bb64acb9873
Malinin Sergey <stron.bery@gmail.com>
2024-09-03 22:17:49 +0300

    Add files via upload
    adding first scripts w/comments

---
+++ input-output.py
@@ -0,0 +1,46 @@
+#data = input()
+#print(data * 5)
+
+#import sys
+#data = sys.stdin.readline()
+#print(data * 5)
+
+#name = input("Введите ваше имя: ")
+#print(f"Приветствую, {name}!")
+
+#Правильный ввод чисел для взаимодействия через Input
+# digit = input("Введите число: ")
+#print(int(digit) / 10)
+
+#Replace
+#ip = input("Введите IP-адрес: ")
+#print(ip.replace(".", "_"))
+
+#Join
+#ip = input("Введите IP-адрес: ")
+#ip = ip.split(".")
+#print("_".join(ip))
+
+#OUTPUT!!!
+#import sys
+#sys.stdout.write("Привет, кожаный мешок!")
+
+#Вывод ответа нескольких Print в одну строку с помощью end=""
+#print("Эта строка плавно переходит", end="")
+#print(" в следующую строку.")
+
+#Кастомизация запятых в ответе Print
+#print(1, 2, 3, 4, 5, 6, 7, sep=" или ")
+
+#Учебный пример
+#num1 = input("Введите первое восьмеричное число: ")
+#num2 = input("Введите второе восьмеричное число: ")
+#num3 = input("Введите третье восьмеричное число: ")
+#num4 = input("Введите четвёртое восьмеричное число: ")
+#print(f"{int(num1, 8)}.{int(num2, 8)}.{int(num3, 8)}.{int(num4, 8)}")
+
+#Учебный пример
+#greeter = input("Введите строку приглашения: ")
+#print(greeter, end="")
+#text = input()
+#print(f"{text}#{text}#{text}")
